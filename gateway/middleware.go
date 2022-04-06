5ceb4602a00696f53de48a323c3b01627506c343
Yann Vigara <yvigara@gmail.com>
2022-04-06 16:30:00 +0100

    Return early and remove else block if tenantName is defined

--- gateway/middleware.go
+++ gateway/middleware.go
@@ -54,47 +54,47 @@ var AuthenticateTenant = middleware.Func(func(next http.Handler) http.Handler {
 		if tenantName != "" {
 			r.Header.Set("X-Scope-OrgID", tenantName)
 			next.ServeHTTP(w, r)
-		} else {
-			logger := klog.With(log.WithContext(r.Context(), log.Logger), "ip_address", r.RemoteAddr)
-			level.Debug(logger).Log("msg", "authenticating request", "route", r.RequestURI)
-
-			if !requestContainsToken(r, headers) {
-				level.Info(logger).Log("msg", "no bearer token provided")
-				http.Error(w, "No bearer token provided", http.StatusUnauthorized)
-				authFailures.WithLabelValues("no_token").Inc()
-				return
-			}
-
-			// Try to parse and validate JWT
-			te := &tenant{}
-			_, err := jwtReq.ParseFromRequest(
-				r,
-				authorizationHeaderExtractor,
-				func(token *jwt.Token) (interface{}, error) {
-					// Only HMAC algorithms accepted - algorithm validation is super important!
-					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-						level.Info(logger).Log("msg", "unexpected signing method", "used_method", token.Header["alg"])
-						return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-					}
-
-					return []byte(jwtSecret), nil
-				},
-				jwtReq.WithClaims(te))
-
-			// If Tenant's Valid method returns false an error will be set as well, hence there is no need
-			// to additionally check the parsed token for "Valid"
-			if err != nil {
-				level.Info(logger).Log("msg", "invalid bearer token", "err", err.Error())
-				http.Error(w, "Invalid bearer token", http.StatusUnauthorized)
-				authFailures.WithLabelValues("token_not_valid").Inc()
-				return
-			}
-
-			// Token is valid
-			authSuccess.WithLabelValues(te.TenantID).Inc()
-			r.Header.Set("X-Scope-OrgID", te.TenantID)
-			next.ServeHTTP(w, r)
+			return
 		}
+		logger := klog.With(log.WithContext(r.Context(), log.Logger), "ip_address", r.RemoteAddr)
+		level.Debug(logger).Log("msg", "authenticating request", "route", r.RequestURI)
+
+		if !requestContainsToken(r, headers) {
+			level.Info(logger).Log("msg", "no bearer token provided")
+			http.Error(w, "No bearer token provided", http.StatusUnauthorized)
+			authFailures.WithLabelValues("no_token").Inc()
+			return
+		}
+
+		// Try to parse and validate JWT
+		te := &tenant{}
+		_, err := jwtReq.ParseFromRequest(
+			r,
+			authorizationHeaderExtractor,
+			func(token *jwt.Token) (interface{}, error) {
+				// Only HMAC algorithms accepted - algorithm validation is super important!
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					level.Info(logger).Log("msg", "unexpected signing method", "used_method", token.Header["alg"])
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+
+				return []byte(jwtSecret), nil
+			},
+			jwtReq.WithClaims(te))
+
+		// If Tenant's Valid method returns false an error will be set as well, hence there is no need
+		// to additionally check the parsed token for "Valid"
+		if err != nil {
+			level.Info(logger).Log("msg", "invalid bearer token", "err", err.Error())
+			http.Error(w, "Invalid bearer token", http.StatusUnauthorized)
+			authFailures.WithLabelValues("token_not_valid").Inc()
+			return
+		}
+
+		// Token is valid
+		authSuccess.WithLabelValues(te.TenantID).Inc()
+		r.Header.Set("X-Scope-OrgID", te.TenantID)
+		next.ServeHTTP(w, r)
 
 	})
 })
